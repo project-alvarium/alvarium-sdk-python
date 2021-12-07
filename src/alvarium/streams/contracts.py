fd88f789adf86968abb227f36c44ad037192e48f
ali ghazal <ali_ghazal@dell.com>
2021-12-07 23:35:11 +0200

    Implement MQTT StreamProvider
    	* created MQTTconfig Dataclass
    	* added case in the streamprovider factory
    	* implemeted the mqtt stream provider
    	* created the needed testing  -- it is commented -- uncomment it and edit the config-mqtt.json file for it to work
    	* edited the config-mqtt.json file to work with the localhost
    
    Signed-off-by: ali ghazal <ali_ghazal@dell.com>

--- src/alvarium/streams/contracts.py
+++ src/alvarium/streams/contracts.py
@@ -1,9 +1,7 @@
 import json
-
 from enum import Enum
 from dataclasses import dataclass
 from typing import Any
-
 
 class StreamType(Enum):
 
@@ -22,9 +20,12 @@ class StreamInfo:
 
     @staticmethod
     def from_json(data: str):
-        # TODO (karim elghamry): add  type check to config and parse accordingly
         info_json = json.loads(data)
-        return StreamInfo(type=StreamType(info_json["type"]), config=info_json["config"])
+        if (info_json["type"] == str(StreamType.MOCK)):
+            return StreamInfo(type=StreamType(info_json["type"]), config=info_json["config"])
+        elif( info_json["type"] == str(StreamType.MQTT)):
+            return StreamInfo(type=StreamType(info_json["type"]), config=MQTTConfig.from_json( json.dumps(info_json["config"])))
+
     
     def to_json(self) -> str:
         info_json = {"type": str(self.type), "config": json.loads(str(self.config))}
@@ -53,5 +54,43 @@ class ServiceInfo:
         info_json = {"host": self.host, "protocol": self.protocol, "port": self.port}
         return json.dumps(info_json)
     
+    def __str__(self) -> str:
+        return self.to_json()
+
+
+@dataclass
+class MQTTConfig:
+    """A data class that encapsulates the MQTT Configuration"""
+
+    clientId: str
+    user: str
+    password: str
+    qos: int
+    isClean: bool
+    topics: list
+    provider: ServiceInfo
+
+    @staticmethod
+    def from_json(data: str):
+        info_json = json.loads(data)
+        return MQTTConfig(clientId=info_json["clientId"],
+                          user=info_json["user"],
+                          password=info_json["password"],
+                          qos=info_json["qos"],
+                          isClean=info_json["cleanness"],
+                          topics=info_json["topics"],
+                          provider= ServiceInfo.from_json ( json.dumps (info_json["provider"])))
+
+    def to_json(self) -> str:
+        info_json = {"clientId": self.clientId,
+                     "user": self.user,
+                     "password": self.password,
+                     "qos": self.qos,
+                     "cleanness": self.isClean,
+                     "topics": self.topics,
+                     "provider": json.loads(self.provider.to_json())
+                     }
+        return json.dumps(info_json)
+
     def __str__(self) -> str:
         return self.to_json()
--- src/alvarium/streams/factories.py
+++ src/alvarium/streams/factories.py
@@ -1,5 +1,6 @@
 from alvarium.streams.exceptions import StreamException
 from alvarium.streams.mock import MockProvider
+from alvarium.streams.mqtt_stream_provider import MQTTStreamProvider
 from .contracts import StreamInfo, StreamType
 
 class StreamProviderFactory:
@@ -8,5 +9,7 @@ class StreamProviderFactory:
     def getProvider(self, info: StreamInfo):
         if info.type == StreamType.MOCK:
             return MockProvider()
+        if info.type == StreamType.MQTT:
+            return MQTTStreamProvider(info.config)
         else:
             raise StreamException(f"{info.type} is not yet implemented.")
