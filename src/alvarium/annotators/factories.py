490ad2563012659fc7a8e8886fd28ff2c09034b1
waleed_badr <waleed_badr@dell.com>
2021-12-13 10:19:12 +0000

    Implement Annotator for TLS Validation
    close #27
    
    * Created Tls annotator
    * Implemented function execute
    * Check if socket is passed correctly
    * Verify client handshake
    * Implemented unit tests for tls annotator
    * added tls annotator rype to annotator factory
    
    Signed-off-by: waleed_badr <waleed_badr@dell.com>

--- src/alvarium/annotators/factories.py
+++ src/alvarium/annotators/factories.py
@@ -1,4 +1,5 @@
 from .interfaces import Annotator
+from alvarium.annotators.tls_anotator import TlsAnotator
 from alvarium.contracts.annotation import AnnotationType
 from alvarium.sign.contracts import SignInfo
 from alvarium.hash.contracts import HashType
@@ -17,6 +18,8 @@ class AnnotatorFactory():
             return TpmAnnotator(hash=hash, sign_info=signature)
         elif kind == AnnotationType.SOURCE:
             return SourceAnnotator(hash=hash, sign_info=signature)
+        elif kind == AnnotationType.TLS:
+            return TlsAnotator(hash=hash, signature=signature)
         else:
             raise AnnotatorException("Annotator type is not supported")
             
---
+++ src/alvarium/annotators/tls_anotator.py
@@ -0,0 +1,56 @@
+import socket
+import ssl
+from alvarium.annotators.exceptions import AnnotatorException
+from alvarium.contracts.annotation import Annotation, AnnotationType
+from alvarium.hash.exceptions import HashException
+from alvarium.hash.contracts import HashType
+from alvarium.sign.contracts import SignInfo
+from .utils import derive_hash, sign_annotation
+from .interfaces import Annotator
+from alvarium.utils import PropertyBag
+import socket
+
+
+class TlsAnotator(Annotator):
+    hash: HashType
+    signature: SignInfo
+    kind: AnnotationType
+
+    def __init__(self, hash: HashType, signature: SignInfo):
+        self.hash = hash
+        self.signature = signature
+        self.kind = AnnotationType.TLS
+    
+    def execute(self, ctx: PropertyBag, data: bytes) -> Annotation:
+        try:
+            key = derive_hash(self.hash, data)
+            is_satisfied = False
+            
+            context = ctx.getProperty(str(AnnotationType.TLS))
+
+            if context != None:
+                # If none is returned then there is no error in handshake so tls is satisfied
+                if  type(context) == ssl.SSLSocket:
+                    try:
+                        if context.do_handshake(block=True) == None:
+                            is_satisfied = True
+                    except:
+                        pass
+
+            
+            annotation = Annotation(key= key, hash= self.hash, host= socket.gethostname(), kind= self.kind, is_satisfied= is_satisfied)
+
+            annotation_signture = sign_annotation(self.signature.private, annotation)
+
+            annotation.signature = str(annotation_signture)
+
+            return annotation
+
+        except HashException as e:
+            raise AnnotatorException("failed to hash data", e)
+        
+        except socket.herror as e:
+            raise AnnotatorException("could not get hostname", e)
+
+
+
